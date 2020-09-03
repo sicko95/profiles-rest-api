ba1b9f2ff7e189e91b46a9eceaaa6098100cd13f
Veljko <sicko1995velja@gmail.com>
2020-09-03 17:23:21 +0200

    Added POST, PUT, PATCH and DELETE method to our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status #Lista status kodova
+
+from . import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIViews features"""
@@ -16,3 +20,32 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+
+        #serializer objektu dodeljujemo definisanu serializer kalsu sa podacima koje zelimo da prosledimo
+        serializers = self.serializer_class(data=request.data)
+
+        #Vrsimo proveru da li je serializer validan
+        if serializers.is_valid():
+            #Kreiramo atribut koji dobija vrednost validiranog imena iz serializer
+            name = serializers.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            #Vraca sve errore koji su se pojavili tokom validacije
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
